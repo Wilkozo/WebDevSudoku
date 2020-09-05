ddc4bd036cababe787783b6328e75b7640c54c26
Wilkozo <52981630+Wilkozo@users.noreply.github.com>
2020-09-05 14:13:24 +1200

    Add files via upload
    HTML Update and JavaScript Intergrations

---
+++ jsBasic.js
@@ -0,0 +1,27 @@
+var posts = [
+    {
+        title: "Why Looting should be legalised",
+        author: "NPC Journalist #253",
+        comments: ["Sick!", "You are the reason for cancer"],
+    },
+
+    {
+        title: "2D is better, Change my mind",
+        author: "Vaugan",
+        comments: ["Stunning & Brave", "You're just a misogonist"],
+    }
+];
+
+var randomObject = {
+    age: 23,
+    color: "Green",
+    isHungry: true,
+    frien: ["Horton", "Friends?"],
+
+    pet: {
+        name: "Doge",
+        species: "Dog",
+        age: 2,
+    }
+}
+console.log(randomObject.pet.name);
