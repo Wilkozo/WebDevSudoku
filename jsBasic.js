9a6b3ef699cb94776aafd451010cdff9b7ab3609
Ryan <mstwilko@gmail.com>
2020-09-15 21:51:28 +1200

    F Update

--- jsBasic.js
+++ jsBasic.js
@@ -1,12 +1,12 @@
 // Variables created for game
 var Difficulty = ["Easy","Normal","Hard"];
-var SBoard = [81] = 0; // Sudoku Board
 var BrdSolved = false;
 const Z = 0;
+let rowoffset = 0;
 
 
 // Easy
-const Board_01 = [
+Board_01 = [
     [Z, Z, Z, Z, Z, Z, Z, Z, Z],
     [Z, Z, Z, Z, Z, Z, Z, Z, Z],
     [Z, Z, Z, Z, Z, Z, Z, Z, Z],
@@ -20,197 +20,208 @@ const Board_01 = [
 
 
 // Normal
-const Board_02 = [
-    [7, Z, Z, Z, Z, Z, Z, Z, Z],
-    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
-    [Z, Z, Z, Z, Z, Z, Z, 6, Z],
+Board_02 = [
     [Z, Z, Z, Z, Z, Z, Z, Z, Z],
     [Z, Z, Z, Z, Z, Z, Z, Z, Z],
-    [Z, Z, 3, Z, Z, Z, Z, Z, Z],
     [Z, Z, Z, Z, Z, Z, Z, Z, Z],
     [Z, Z, Z, Z, Z, Z, Z, Z, Z],
-    [Z, Z, Z, Z, Z, 4, Z, Z, Z]
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z]
 ]
 
 
 function ClearBoard()
 {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            Board_01[i][j] = Z; // Sets to Zero
+        }
+    }
+}
+
+function DisplayBoard() 
+{
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            $("#" + i + j).val(Board_01[i][j]);
+        }
+    }
+}
+
+function CheckBoardSolvable() {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (Board_01[j][i] != 0) {
+                if (CheckNumber(i, j, Board_01[j][i]) == false) {
+                    return false;
+                }
+            }
+        }
+    }
+}
+
+function SeedBoard() {
+
+    for (var i = 0; i < 9; i++) {
+
+        let x = Math.floor(Math.random() * 9);
+        let y = Math.floor(Math.random() * 9);
+        let number = Math.floor(Math.random() * 9) + 1;
+
+        if (CheckNumber(x, y, number)) {
+            Board_01[y][x] = number;
+        }
+    }
+
+    Board_02 = Board_01;
+
+    if (CheckBoardSolvable()) {
+        ClearBoard()
+        SeedBoard()
+    }
+
+    Board_01 = Board_02;
+
+}
+
+
+function CheckNumber(x, y, number)
+{
+
+    for(var i = 0; i < 9; i++)
+    {
+        if (Board_01[i][x] == number) {
+            return false;
+        }
+    }
+
+    for (var j = 0; j < 9; j++) {
+        if (Board_01[y][j] == number) {
+            return false;
+        }
+    }
+    
+    let Lx = 0;
+    let Ly = 0;
+
+    if (x < 3) {
+        Lx = 0;
+    }
+    else if (x < 6) {
+        Lx = 3;
+    }
+    else {
+        Lx = 6;
+    }
+
+    if (y < 3) {
+        Ly = 0;
+    }
+    else if (y < 6) {
+        Ly = 3;
+    }
+    else {
+        Ly = 6;
+    }
+
+    for (var i = 0; i < 3; i++) 
+    {
+        for (var j = 0; j < 3; j++) 
+        {
+            if (Board_01[i + Ly][j + Lx] == number)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function BoardSolved() {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (Board_01[i][j] == 0) {
+                return false;
+            }//CheckNumber(i, j, Board_01[i][j]) == false)
+
+        }
+    }
+    if (SolveBoard() == false) {
+      return false;
+    }
+    return true;
+}
+
+function SolveBoard()
+{
+    for (var i = 0; i < 9; i++) 
+    {
+        for (var j = 0; j < 9; j++) 
+        {
+            if (Board_01[j][i] == 0) 
+            {
+                for (var n = 1; n < 10; n++)
+                {
+                    if (CheckNumber(i, j, n)) 
+                    {
+                        // Sets Slot to number
+                        Board_01[j][i] = n;
+                        if (SolveBoard()) 
+                        { return true; }
+                        // Sets slot to empty
+                        Board_01[j][i] = 0;
+
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
     
 
-}
-
-
-function InitialiseBoard()
-{
-    var m_sboard; // seedboard function;
-    //m_sboard =  solve function
-
-}
-
-
-function SeedBoard()
-{
-
-}
-
-
-function SolveBoard(SBoard)
-{
-    // Checks if the board has already been solved...
-    if (SolveBoard(SBoard))
-    {return SBoard}
-    else
-    {
-        const Solutions = nextGrids(SBoard) // Gets possible solutions
-        const ValidSolutions = PurgeBadSolutions(Solutions) // Pruning
-        return FindSolution(ValidSolutions) // returns the solution 
-    }
-
-}
-
-
-function FindSolution(SBoard)
-{
-    // checks if the board length
-    if (SBoard.length < 1) 
-    {return false}
-    else
-    {
-        // backtrack the board for an answer...
-        var first = SBoard.shift() // gets first element
-        const TryPath = SolveBoard(first)
-        // If it fails
-        if (TryPath != false)
-        { return TryPath}
-        else
-        { return FindSolution(SBoard)}
-    }
-}
-
-
-function CheckSolved(SBoard)
-{
-    // Rows
-    for (var i = 0; i < 9; i++)
-    {
-        // Columns
-        for (var j = 0; j< 9; j++)
-        {
-            if (board[i][j] === null)
-            {
-                return false
-            }
-        }
-    }
-    return true
-}
-
-
-function nextGrids(SBoard)
-{
-    var result = []
-    const firstBlank = findEmptySlot(SBoard) // should return Coords 
-    if (firstBlank != undefined)
-    {
-        const y = firstBlank[0]
-        const x = firstBlank[1]
-        for (var i = 1; i <= 9; i++)
-        {
-            var tmpBoard = [...SBoard]
-            var row = [...tmpBoard[y]]
-            row[x] = i
-            tmpBoard[y] = row
-            result.push(tmpBoard)
-        }
-    }
-    return result
-}
-
-
-function findBlankSlot(SBoard)
-{
-    for (var i = 0; i < 9; i++)
-    {
-        for (var j = 0; j < 9; j++)
-        {
-            if (SBoard[i][j] == null)
-            {
-                return [i,j]
-            }
-        }
-    }
-}
-
-
-function PurgeBadSolutions(SBoard)
-{return SBoard.filter(Z => ValidGrid(Z))}
-
-
-function ValidGrid(SBoard)
-{return ValidRow(SBoard) && ValidColumn(SBoard) && ValidGrids(SBoard)}
-
-function ValidRow(SBoard)
-{
-    for (var i = 0; i < 9; i++)
-    {
-        var current = []
-        for (var j = 0; j < 9; j++)
-        {
-            if (current.includes(SBoard[i][j]))
-            {
-                return false
-            }
-            else if (SBoard[i][j] != null)
-            {current.push(SBoard[i][j])}
-        }
-    }
-    return true
-}
-
-function ValidColumn(SBoard)
-{
-    for (var i = 0; i < 9; i++)
-    {
-        var current = []
-        for (var j = 0; j < 9; j++)
-        {
-            if (current.includes(SBoard[j][i]))
-            {
-                return false
-            }
-            else if (SBoard[i][j] != null)
-            {current.push(SBoard[j][i])}
-        }
-    }
-    return true
-}
-
-function ValidGrids(SBoard)
-{
-    const GridCoords = [
-        [0, 0], [0,1], [0,2],
-        [1, 0], [1,1], [1,2],
-        [2, 0], [2,1], [2,2]
-    ]
+    console.log("Solved!");
+    return true;
     
-    for (var y = 0; y < 9; y+=3)
-    {
-        for (var x = 0; x < 9; x+=3)
-        {
-            var current = []
-            for (var i = 0; i < 9; i++)
-            {
-                var Coords = [...GridCoords[i]]
-                Coords[0] += y
-                Coords[1] += x
-                if (current.includes(SBoard[Coords[0]][Coords[1]]))
-                {return false}
-                else if (SBoard[Coords[0]][Coords[1]] != null)
-                {current.push(SBoard[Coords[0]][Coords[1]])}
-            }
-        }
-    }
-    return true
 }
 
-console.log(solve(Board_01))
+
+function updateInput(position) {
+
+    let val = $("#" + position).val();
+    if (val.length > 1) {
+        val = val[1];
+    }
+    Board_01[position[0]][position[1]] = val;
+    $("#" + position).val(val);
+}
+
+$("#New").on("click", function () 
+{
+    ClearBoard();
+    SeedBoard();
+    DisplayBoard();
+});
+
+$("#Solve").on("click", function () 
+{
+    SolveBoard();
+    DisplayBoard();
+});
+
+$("#Check").on("click", function () 
+{
+    if (BoardSolved() == true)
+    {$("h2").html("You Win")}
+});
+
+$(document).ready(function ()
+{
+    ClearBoard();
+    SeedBoard();
+    DisplayBoard();
+})
