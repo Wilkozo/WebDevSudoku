32755d030762052ebb2c6eb607475011598d28c7
Wilkozo <52981630+Wilkozo@users.noreply.github.com>
2020-09-15 17:57:41 +1200

    Add files via upload

--- jsBasic.js
+++ jsBasic.js
@@ -1,27 +1,216 @@
-var posts = [
-    {
-        title: "Why Looting should be legalised",
-        author: "NPC Journalist #253",
-        comments: ["Sick!", "You are the reason for cancer"],
-    },
+// Variables created for game
+var Difficulty = ["Easy","Normal","Hard"];
+var SBoard = [81] = 0; // Sudoku Board
+var BrdSolved = false;
+const Z = 0;
 
+
+// Easy
+const Board_01 = [
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z]
+]
+
+
+// Normal
+const Board_02 = [
+    [7, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, 6, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, 3, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, Z, Z, Z, Z],
+    [Z, Z, Z, Z, Z, 4, Z, Z, Z]
+]
+
+
+function ClearBoard()
+{
+    
+
+}
+
+
+function InitialiseBoard()
+{
+    var m_sboard; // seedboard function;
+    //m_sboard =  solve function
+
+}
+
+
+function SeedBoard()
+{
+
+}
+
+
+function SolveBoard(SBoard)
+{
+    // Checks if the board has already been solved...
+    if (SolveBoard(SBoard))
+    {return SBoard}
+    else
     {
-        title: "2D is better, Change my mind",
-        author: "Vaugan",
-        comments: ["Stunning & Brave", "You're just a misogonist"],
+        const Solutions = nextGrids(SBoard) // Gets possible solutions
+        const ValidSolutions = PurgeBadSolutions(Solutions) // Pruning
+        return FindSolution(ValidSolutions) // returns the solution 
     }
-];
 
-var randomObject = {
-    age: 23,
-    color: "Green",
-    isHungry: true,
-    frien: ["Horton", "Friends?"],
+}
 
-    pet: {
-        name: "Doge",
-        species: "Dog",
-        age: 2,
+
+function FindSolution(SBoard)
+{
+    // checks if the board length
+    if (SBoard.length < 1) 
+    {return false}
+    else
+    {
+        // backtrack the board for an answer...
+        var first = SBoard.shift() // gets first element
+        const TryPath = SolveBoard(first)
+        // If it fails
+        if (TryPath != false)
+        { return TryPath}
+        else
+        { return FindSolution(SBoard)}
     }
 }
-console.log(randomObject.pet.name);
+
+
+function CheckSolved(SBoard)
+{
+    // Rows
+    for (var i = 0; i < 9; i++)
+    {
+        // Columns
+        for (var j = 0; j< 9; j++)
+        {
+            if (board[i][j] === null)
+            {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+
+function nextGrids(SBoard)
+{
+    var result = []
+    const firstBlank = findEmptySlot(SBoard) // should return Coords 
+    if (firstBlank != undefined)
+    {
+        const y = firstBlank[0]
+        const x = firstBlank[1]
+        for (var i = 1; i <= 9; i++)
+        {
+            var tmpBoard = [...SBoard]
+            var row = [...tmpBoard[y]]
+            row[x] = i
+            tmpBoard[y] = row
+            result.push(tmpBoard)
+        }
+    }
+    return result
+}
+
+
+function findBlankSlot(SBoard)
+{
+    for (var i = 0; i < 9; i++)
+    {
+        for (var j = 0; j < 9; j++)
+        {
+            if (SBoard[i][j] == null)
+            {
+                return [i,j]
+            }
+        }
+    }
+}
+
+
+function PurgeBadSolutions(SBoard)
+{return SBoard.filter(Z => ValidGrid(Z))}
+
+
+function ValidGrid(SBoard)
+{return ValidRow(SBoard) && ValidColumn(SBoard) && ValidGrids(SBoard)}
+
+function ValidRow(SBoard)
+{
+    for (var i = 0; i < 9; i++)
+    {
+        var current = []
+        for (var j = 0; j < 9; j++)
+        {
+            if (current.includes(SBoard[i][j]))
+            {
+                return false
+            }
+            else if (SBoard[i][j] != null)
+            {current.push(SBoard[i][j])}
+        }
+    }
+    return true
+}
+
+function ValidColumn(SBoard)
+{
+    for (var i = 0; i < 9; i++)
+    {
+        var current = []
+        for (var j = 0; j < 9; j++)
+        {
+            if (current.includes(SBoard[j][i]))
+            {
+                return false
+            }
+            else if (SBoard[i][j] != null)
+            {current.push(SBoard[j][i])}
+        }
+    }
+    return true
+}
+
+function ValidGrids(SBoard)
+{
+    const GridCoords = [
+        [0, 0], [0,1], [0,2],
+        [1, 0], [1,1], [1,2],
+        [2, 0], [2,1], [2,2]
+    ]
+    
+    for (var y = 0; y < 9; y+=3)
+    {
+        for (var x = 0; x < 9; x+=3)
+        {
+            var current = []
+            for (var i = 0; i < 9; i++)
+            {
+                var Coords = [...GridCoords[i]]
+                Coords[0] += y
+                Coords[1] += x
+                if (current.includes(SBoard[Coords[0]][Coords[1]]))
+                {return false}
+                else if (SBoard[Coords[0]][Coords[1]] != null)
+                {current.push(SBoard[Coords[0]][Coords[1]])}
+            }
+        }
+    }
+    return true
+}
+
+console.log(solve(Board_01))
